3e05f8f13cc5d0f7b9676ea62260d210689b4eff
untakersns <untaker1games@gmail.com>
2025-10-05 17:10:15 +0300

    Сделаны блоки с криптой. Сделан поиск крипты. Все подключено к api. TODO: - сделать кнопки удаления блоков - сделать графики цен

---
+++ cryptovalutchik/Components/Types/Cryptocurrency.cs
@@ -0,0 +1,8 @@
+﻿namespace cryptovalutchik.Components.Types;
+public class Cryptocurrency()
+{
+    public Guid id {  get; init; }
+    public string ticker { get; init; }
+    public string name {  get; init; }
+    public double rate {  get; init; }
+}
